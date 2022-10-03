3a36818ce1ab0d75b4333e3350dc013d5ddbd0a7
Ismael Celis <ismaelct@gmail.com>
2022-10-03 09:27:58 +0100

    Test expected_seq

--- spec/service_spec.cr
+++ spec/service_spec.cr
@@ -24,32 +24,34 @@ describe SourcedStore::Service do
 
   describe "#append_to_stream, #read_stream" do
     stream_id = "test-stream-1"
+    request_events = [
+      build_event(
+        id: UUID.random.to_s,
+        topic: "orders.start",
+        stream_id: stream_id,
+        originator_id: nil,
+        seq: 1,
+        created_at: Google::Protobuf::Timestamp.new(seconds: 1664718079, nanos: 0),
+        payload: nil
+      ),
+      build_event(
+        id: UUID.random.to_s,
+        topic: "orders.place",
+        stream_id: stream_id,
+        originator_id: nil,
+        seq: 2,
+        created_at: Google::Protobuf::Timestamp.new(seconds: 1664718080, nanos: 0),
+        payload: nil
+      ),
+    ] of SourcedStore::TwirpTransport::Event
+
     req = SourcedStore::TwirpTransport::AppendToStreamRequest.new(
       stream_id: stream_id,
       expected_seq: 2,
-      events: [
-        build_event(
-          id: UUID.random.to_s,
-          topic: "orders.start",
-          stream_id: stream_id,
-          originator_id: nil,
-          seq: 1,
-          created_at: Google::Protobuf::Timestamp.new(seconds: 1664718079, nanos: 0),
-          payload: nil
-        ),
-        build_event(
-          id: UUID.random.to_s,
-          topic: "orders.place",
-          stream_id: stream_id,
-          originator_id: nil,
-          seq: 2,
-          created_at: Google::Protobuf::Timestamp.new(seconds: 1664718080, nanos: 0),
-          payload: nil
-        ),
-      ] of SourcedStore::TwirpTransport::Event
+      events: request_events
     )
 
-    it "works" do
+    it "appends and reads events" do
       resp = service.append_to_stream(req: req)
       resp.should be_a(SourcedStore::TwirpTransport::AppendToStreamResponse)
       resp.successful.should eq(true)
@@ -65,6 +67,28 @@ describe SourcedStore::Service do
       sent_events = req.events.as(Array(SourcedStore::TwirpTransport::Event))
       events.size.should eq(2)
       events.first.should eq(sent_events.first)
+    end
+
+    it "fails if expected_seq doesn't match" do
+      service.append_to_stream(req: req)
+      new_req = SourcedStore::TwirpTransport::AppendToStreamRequest.new(
+        stream_id: stream_id,
+        expected_seq: 2,
+        events: [
+          build_event(
+            id: UUID.random.to_s,
+            topic: "orders.close",
+            stream_id: stream_id,
+            originator_id: nil,
+            seq: 2,
+            created_at: Google::Protobuf::Timestamp.new(seconds: 1664718080, nanos: 0),
+            payload: nil
+          ),
+        ] of SourcedStore::TwirpTransport::Event
+      )
+      resp = service.append_to_stream(req: new_req)
+      resp.successful.should eq(false)
+      resp.error.should eq("duplicate key value violates unique constraint \"unique_index_on_entity_seqs\"")
     end
   end
 end
