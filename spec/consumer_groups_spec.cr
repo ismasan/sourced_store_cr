9e1b57bfd9eb07bb377e8f2ae9a72bab5def6504
Ismael Celis <ismaelct@gmail.com>
2022-10-04 12:42:37 +0100

    WiP ConsumerGroups

---
+++ spec/consumer_groups_spec.cr
@@ -0,0 +1,44 @@
+require "logger"
+require "./spec_helper"
+require "../src/consumer_groups"
+
+describe SourcedStore::ConsumerGroups do
+  logger = Logger.new(STDOUT, level: Logger::INFO)
+  groups = uninitialized SourcedStore::ConsumerGroups
+
+  before_each do
+    groups = SourcedStore::ConsumerGroups.new(logger)
+  end
+
+  describe "#register" do
+    it "registers and groups consumers, assigning them numbers" do
+      c1 = groups.register("group-1", "c1")
+      c2 = groups.register("group-1", "c2")
+      c1b = groups.register("group-1", "c1")
+
+      c1.number.should eq(1)
+      c2.number.should eq(2)
+      c1b.number.should eq(1)
+      c1.should eq(c1b)
+      c1.group_name.should eq("group-1")
+      c2.group_name.should eq("group-1")
+    end
+  end
+
+  describe "#notify_consumer" do
+    it "updates consumers with last_global_seq and keeps track of them" do
+      c1 = groups.register("group-1", "c1")
+      c2 = groups.register("group-1", "c2")
+
+      groups.notify_consumer(c1, 2)
+      groups.notify_consumer(c2, 4)
+      groups.notify_consumer(c1, 5)
+
+      c1.last_global_seq.should eq(5)
+      c2.last_global_seq.should eq(4)
+
+      groups.minimum_global_seq_for("group-1").should eq(4)
+      groups.minimum_global_seq_for("nope").should eq(0)
+    end
+  end
+end
---
+++ src/consumer_groups.cr
@@ -0,0 +1,83 @@
+require "logger"
+
+module SourcedStore
+  class ConsumerGroups
+    ZERO64 = Int64.new(0)
+
+    class Group
+      getter name : String
+
+      def initialize(@name)
+        @consumers = Hash(String, Consumer).new
+        @count = 0
+      end
+
+      def register(consumer_id : String)
+        consumer = @consumers[consumer_id]?
+        return consumer if consumer
+
+        @count += 1
+        consumer = Consumer.new(group_name: name, id: consumer_id, number: @count)
+        @consumers[consumer_id] = consumer
+        consumer
+      end
+
+      def minimum_global_seq : Int64
+        return ZERO64 unless @consumers.any?
+
+        @consumers.values.sort_by { |c| c.last_global_seq }.first.last_global_seq
+      end
+    end
+
+    class Consumer
+      getter group_name : String
+      getter id : String
+      getter number : Int32
+      getter last_global_seq : Int64
+
+      def initialize(@group_name, @id, @number)
+        @last_global_seq = ZERO64
+      end
+
+      def notify(seq : Int64) : Consumer
+        @last_global_seq = seq
+        self
+      end
+    end
+
+    def initialize(logger : Logger)
+      @groups = Hash(String, Group).new
+      @lock = Mutex.new
+      @logger = logger
+    end
+
+    def register(group_name : String, consumer_id : String, &block)
+      group = register(group_name, consumer_id)
+      yield group
+    end
+
+    def register(group_name : String, consumer_id : String) : Consumer
+      @lock.synchronize do
+        group = @groups[group_name]? || Group.new(name: group_name)
+        consumer = group.register(consumer_id)
+        @groups[group_name] = group
+        consumer
+      end
+    end
+
+    def notify_consumer(consumer : Consumer, last_global_seq : Int64) : Consumer
+      register(consumer.group_name, consumer.id) do |cn|
+        cn.notify(last_global_seq)
+        @logger.info "consumer #{cn.group_name}/#{cn.id} (#{cn.number}) on #{cn.last_global_seq}"
+        cn
+      end
+    end
+
+    def minimum_global_seq_for(group_name : String) : Int64
+      @lock.synchronize do
+        group = @groups[group_name]?
+        group ? group.minimum_global_seq : ZERO64
+      end
+    end
+  end
+end
