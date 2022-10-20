91c99e8f98bcb5f5e7e45a78834b311c96bf7a7c
Ismael Celis <ismaelct@gmail.com>
2022-10-20 09:59:25 +0100

    WiP sourced

--- stage.cr
+++ stage.cr
@@ -1,48 +1,76 @@
-module EventSourcing
+module Sourced
   class Event
     alias Seq = Int64
     getter seq : Seq = Int64.new(0)
+
+    class Payload
+
+    end
   end
 
   alias EventList = Array(Event)
 
-  macro event(class_name, *properties)
-    class {{class_name}} < EventSourcing::Event
-      {% for property in properties %}
-        getter {{property}}
-      {% end %}
+  module Macros
+    macro event(class_name, *properties)
+      class {{class_name}} < Sourced::Event
+        class Payload < Sourced::Event::Payload
+          {% for property in properties %}
+            getter {{property}}
+          {% end %}
 
-      def initialize(@seq, {{
-         *properties.map do |field|
-           "@#{field.id}".id
-         end
-       }})
-      end
+          def initialize({{
+             *properties.map do |field|
+               "@#{field.id}".id
+             end
+           }})
+          end
+        end
 
-      def initialize({{
-         *properties.map do |field|
-           "@#{field.id}".id
-         end
-       }})
-      end
+        getter payload : Payload
 
-      def with_seq(seq : EventSourcing::Event::Seq)
-        self.class.new(seq, {{
-         *properties.map do |field|
-           "#{field.var.id}".id
-         end
-        }})
+        def initialize(seq : Sourced::Event::Seq | Int32, {{
+           *properties.map do |field|
+             "#{field.id}".id
+           end
+         }})
+          @seq = seq.is_a?(Sourced::Event::Seq) ? seq : Int64.new(seq)
+          @payload = Payload.new({{
+           *properties.map do |field|
+             "#{field.var.id}".id
+           end
+          }})
+        end
+
+        def initialize({{
+           *properties.map do |field|
+             "#{field.id}".id
+           end
+         }})
+          @payload = Payload.new({{
+           *properties.map do |field|
+             "#{field.var.id}".id
+           end
+          }})
+        end
+
+        def initialize(@seq, @payload : Payload)
+
+        end
+
+        def with_seq(seq : Sourced::Event::Seq)
+          self.class.new(seq, payload)
+        end
       end
     end
   end
 
   class Projector(T)
-    def call(entity : T, evt : EventSourcing::Event) : T
+    def call(entity : T, evt : Sourced::Event) : T
       _apply(entity, evt)
       entity
     end
 
-    def _apply(entity : T, evt : EventSourcing::Event)
+    def _apply(entity : T, evt : Sourced::Event)
       puts "Unhandled event #{evt.inspect}"
     end
   end
@@ -50,13 +78,13 @@ module EventSourcing
   class Stage(T, P)
     getter stream_id : String
     getter entity : T
-    getter seq : EventSourcing::Event::Seq
-    getter last_committed_seq : EventSourcing::Event::Seq
-    getter uncommitted_events : Array(EventSourcing::Event)
+    getter seq : Sourced::Event::Seq
+    getter last_committed_seq : Sourced::Event::Seq
+    getter uncommitted_events : Array(Sourced::Event)
     getter projector : P
 
     # Load from stream
-    def initialize(stream_id : String, entity : T, projector : P, stream : EventSourcing::EventList)
+    def initialize(stream_id : String, entity : T, projector : P, stream : Sourced::EventList)
       seq = Int64.new(0)
       stream.each do |evt|
         entity = projector.call(entity, evt)
@@ -66,12 +94,12 @@ module EventSourcing
       initialize(stream_id, entity, projector, seq)
     end
 
-    def initialize(@stream_id : String, @entity : T, @projector : P, @seq : EventSourcing::Event::Seq = Int64.new(0))
+    def initialize(@stream_id : String, @entity : T, @projector : P, @seq : Sourced::Event::Seq = Int64.new(0))
       @last_committed_seq = @seq
-      @uncommitted_events = Array(EventSourcing::Event).new
+      @uncommitted_events = Array(Sourced::Event).new
     end
 
-    def apply(evt : EventSourcing::Event)
+    def apply(evt : Sourced::Event)
       evt = evt.with_seq(seq + 1)
       @entity = projector.call(entity, evt)
       @seq = evt.seq
@@ -80,34 +108,38 @@ module EventSourcing
   end
 end
 
-class User
-  property name : String = ""
-  property age : Int32 = 0
-end
+module App
+  include Sourced::Macros
 
-EventSourcing.event NameUpdated, new_name : String
-EventSourcing.event AgeUpdated, new_age : Int32
-
-class UserProjector < EventSourcing::Projector(User)
-  def _apply(user : User, evt : NameUpdated)
-    user.name = evt.new_name
+  class User
+    property name : String = ""
+    property age : Int32 = 0
   end
 
-  def _apply(user : User, evt : AgeUpdated)
-    user.age = evt.new_age
-  end
-end
+  event NameUpdated, new_name : String
+  event AgeUpdated, new_age : Int32
 
-class UserStage < EventSourcing::Stage(User, UserProjector)
+  class UserProjector < Sourced::Projector(User)
+    def _apply(user : User, evt : NameUpdated)
+      user.name = evt.payload.new_name
+    end
+
+    def _apply(user : User, evt : AgeUpdated)
+      user.age = evt.payload.new_age
+    end
+  end
+
+  class UserStage < Sourced::Stage(User, UserProjector)
+  end
 end
 
 stream = [
-  NameUpdated.new(new_name: "Joe")
-] of EventSourcing::Event
+  App::NameUpdated.new(seq: 1, new_name: "Joe")
+] of Sourced::Event
 
-user = User.new
+user = App::User.new
 # stage = UserStage.new("g1", user, UserProjector.new)
-stage = UserStage.new("g1", user, UserProjector.new, stream)
-# stage.apply(NameUpdated.new(new_name: "Ismael"))
-stage.apply(AgeUpdated.new(new_age: 44))
+stage = App::UserStage.new("g1", user, App::UserProjector.new, stream)
+stage.apply(App::NameUpdated.new(new_name: "Ismael"))
+stage.apply(App::AgeUpdated.new(new_age: 44))
 puts stage.inspect
